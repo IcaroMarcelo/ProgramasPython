75873f89c9409b7c56712983918b86937a5df3b6
IcaroMarcelo <181748674+IcaroMarcelo@users.noreply.github.com>
2026-06-10 09:09:24 -0300

    feat: adiciona exercícios básicos em Python

---
+++ Secao02/Exercicio02.py
@@ -0,0 +1,9 @@
+#fazer um algoritmo que some dois numeros e multiplique o resultado pelo primeiro numero.
+#entrada
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo numero: "))
+#processamento
+soma = num1 + num2
+multiplicação = soma * num1
+#saida
+print("O resultado da multiplicação é {0}".format(multiplicação))
